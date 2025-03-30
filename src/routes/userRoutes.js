9e27a463a0a7158ef3e101ef734d781717497d2c
Reinigen <raymundcrystal@yahoo.com>
2025-03-30 21:56:09 +0800

    Added user verification

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -8,8 +8,16 @@ router.post("/register", UserController.registerUser);
 router.post("/login", UserController.loginUser);
 router.get("/", UserController.getAllUsers);
 router.get("/details", verifyToken, UserController.getUserDetails);
-router.put("/update-user-details/:id", UserController.updateUserDetails);
-router.patch("/update-user-password/:id", UserController.updateUserPassword);
+router.put(
+  "/update-user-details/:id",
+  verifyToken,
+  UserController.updateUserDetails
+);
+router.patch(
+  "/update-user-password/:id",
+  verifyToken,
+  UserController.updateUserPassword
+);
 router.delete(
   "/delete-user/:id",
   verifyToken,
